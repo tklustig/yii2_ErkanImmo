d985a4a37777d7fb52b19e3f931ec5fd3de95e24
Thomas Kipp <t.kipp@eisvogel-onlnie-software.de>
2018-06-02 19:37:09 +0200

    Tracked

--- common/classes/FrontendAsset.php
+++ common/classes/FrontendAsset.php
@@ -29,7 +29,24 @@ class FrontendAsset extends BaseFrontendAsset {
         'js/scripts.js'
     ];
     public $depends = [
+        'common\classes\AssetBundle',
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+        'yii\bootstrap\BootstrapPluginAsset',
     ];
+    public $skin = '_all-skins';
+
+    public function init() {
+        if ($this->skin) {
+            if (('_all-skins' !== $this->skin) && (strpos($this->skin, 'skin-') !== 0)) {
+                throw new Exception('Invalid skin specified');
+            }
+
+            $this->css[] = sprintf('css/skins/%s.min.css', $this->skin);
+        }
+
+        parent::init();
+    }
 
 }
 ?>
