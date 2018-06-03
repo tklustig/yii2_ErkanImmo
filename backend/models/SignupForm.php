cae8ad6c950d0fd51ccf3b1d62ab1ab4974535a2
Thomas Kipp <t.kipp@eisvogel-onlnie-software.de>
2018-06-03 23:46:56 +0200

    Bilder in das Backend eingefügt

---
+++ backend/models/SignupForm.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace backend\models;
+
+use yii\base\Model;
+use common\models\User;
+
+/**
+ * Signup form
+ */
+class SignupForm extends Model {
+
+    public $username;
+    public $email;
+    public $password;
+    public $telefon;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules() {
+        return [
+            ['username', 'trim'],
+            ['username', 'required'],
+            ['username', 'unique', 'targetClass' => '\common\models\User', 'message' => 'This username has already been taken.'],
+            ['username', 'string', 'min' => 2, 'max' => 255],
+            ['email', 'trim'],
+            ['email', 'required'],
+            ['email', 'email'],
+            ['email', 'string', 'max' => 255],
+            ['email', 'unique', 'targetClass' => '\common\models\User', 'message' => 'This email address has already been taken.'],
+            ['telefon', 'trim'],
+            ['telefon', 'required'],
+            ['telefon', 'unique', 'targetClass' => '\common\models\User', 'message' => 'This number has already been taken'],
+            ['telefon', 'string', 'min' => 5, 'max' => 255],
+            ['password', 'required'],
+            ['password', 'string', 'min' => 6],
+        ];
+    }
+
+    /**
+     * Signs user up.
+     *
+     * @return User|null the saved model or null if saving fails
+     */
+    public function signup() {
+        if (!$this->validate()) {
+            return null;
+        }
+
+        $user = new User();
+        $user->username = $this->username;
+        $user->email = $this->email;
+        $user->telefon = $this->telefon;
+        $user->setPassword($this->password);
+        $user->generateAuthKey();
+
+        return $user->save() ? $user : null;
+    }
+
+}
