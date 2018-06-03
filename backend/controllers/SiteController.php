97b6835b135e8ca5b93d1ffaf9fa0c474bd201bd
Thomas Kipp <t.kipp@eisvogel-onlnie-software.de>
2018-06-04 00:11:40 +0200

    Signup aus Frontend entfernt

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -74,9 +74,6 @@ class SiteController extends Controller {
      */
     public function actionLogin() {
         $this->layout = "main_login";
-        if (!Yii::$app->user->isGuest) {
-            return $this->goHome();
-        }
         $model = new LoginForm();
         if ($model->load(Yii::$app->request->post()) && $model->login()) {
             return $this->goBack();
