bb9c7cd343d1330618c5f44d9bb34fc0047aa1a3
Thomas Kipp <t.kipp@eisvogel-onlnie-software.de>
2018-06-06 19:04:37 +0200

    Outfit optimert - Teil 1

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -59,7 +59,7 @@ $link = \Yii::$app->urlManagerBackend->baseUrl . '/login';
         <div class="content_wrapper clearfix">
             <div class="sections_group">
                 <div class="extra_content">
-                    <div class="section mcb-section" style="padding-top:180px;padding-bottom:0px; background-color:">
+                    <div class="section mcb-section" style="background-image:url(../img/smoke_men-wallpaper-1680x1050.jpg); background-repeat:no-repeat; background-position:center top;-webkit-background-size:">
                         <div class="section_wrapper mcb-section-inner">
                             <div class="wrap mcb-wrap one  valign-top clearfix" style="">
                                 <div class="mcb-wrap-inner">
@@ -67,154 +67,78 @@ $link = \Yii::$app->urlManagerBackend->baseUrl . '/login';
                                         <div class="column_attr clearfix"  style="">
                                             <h6 class="themecolor" style="text-transform: uppercase;">Startseite</h6>
                                             <h2>Kanat Immobilien PG & Co.KG</h2>
+
+                                            <?php
+//Hier werden alle Flashnachrichten ausgegeben
+                                            $session = new Session();
+                                            if (!empty($session->getAllFlashes())) {
+                                                foreach ($session->getAllFlashes() as $flash) {
+                                                    foreach ($flash as $ausgabe) {
+                                                        ?><?=
+                                                        Alert::widget([
+                                                            'type' => Alert::TYPE_SUCCESS,
+                                                            'title' => 'Information',
+                                                            'icon' => 'glyphicon glyphicon-exclamation-sign',
+                                                            'body' => $ausgabe,
+                                                            'showSeparator' => true,
+                                                            'delay' => false
+                                                        ]);
+                                                    }
+                                                }
+                                            }
+                                            ?>
+
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                    <?php
-//Hier werden alle Flashnachrichten ausgegeben
-                    $session = new Session();
-                    if (!empty($session->getAllFlashes())) {
-                        foreach ($session->getAllFlashes() as $flash) {
-                            foreach ($flash as $ausgabe) {
-                                ?><?=
-                                Alert::widget([
-                                    'type' => Alert::TYPE_SUCCESS,
-                                    'title' => 'Information',
-                                    'icon' => 'glyphicon glyphicon-exclamation-sign',
-                                    'body' => $ausgabe,
-                                    'showSeparator' => true,
-                                    'delay' => false
-                                ]);
-                            }
-                        }
-                    }
-                    ?>
-                    <div class="section the_content no_content">
-                        <div class="section_wrapper">
-                            <div class="the_content_wrapper">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="section ">
-                    <div class="section_wrapper clearfix">
-                        <div class="column one column_portfolio">
-                            <div class="portfolio_wrapper isotope_wrapper">
-                                <ul class="portfolio_group lm_wrapper isotope grid col-2">
-                                    <li class="portfolio-item isotope-item  has-thumbnail">
-                                        <div class="portfolio-item-fw-bg" style="">
-                                            <div class="image_frame scale-with-grid">
-                                                <div class="image_wrapper">
-                                                    <div class="mask"></div>
-                                                    <?= Html::img('@web/img/accept_all_you_see-wallpaper-1680x1050.jpg', ['alt' => 'PicNotFound', 'class' => 'scale-with-grid wp-post-image', 'style' => 'width:960;height:700']); ?>
-                                                    <div class="image_links double">
-                                                        <div class="image_links double">
-                                                            <?= Html::a('', Yii::getAlias('@web') . '/img/pic1.jpg', ['target' => '_blank', 'alt' => 'PicNotFound', 'class' => 'icon-search']) ?>
+
+                            <div class="widgets_wrapper" style="padding:0 0 30px;">
+                                <div class="container">
+                                    <div class="column one">
+                                        <aside id="text-2" class="widget widget_text">
+                                            <div class="textwidget">
+                                                <div style="position: relative; margin-top: -100px; z-index: 1;">
+                                                    <div class="image_frame image_item no_link scale-with-grid aligncenter no_border" >
+                                                        <div class="image_wrapper">
+                                                            <?= Html::img('@web/img/erkan_logo.jpg', ['alt' => 'PicNotFound', 'class' => 'scale-with-grid', 'style' => 'width:350px;height:150px;']); ?>
                                                         </div>
                                                     </div>
                                                 </div>
-                                            </div>
-                                        </div>
-                                    </li>
-                                    <li class="portfolio-item isotope-item  has-thumbnail">
-                                        <div class="portfolio-item-fw-bg" style="">
-                                            <div class="portfolio-item-fill"></div>
-                                            <div class="image_frame scale-with-grid">
-                                                <div class="image_wrapper">
-                                                    <div class="mask"></div>
-                                                    <?= Html::img('@web/img/pic2.jpg', ['alt' => 'PicNotFound', 'class' => 'scale-with-grid wp-post-image']); ?>
-                                                    <div class="image_links double">
-                                                        <?= Html::a('', Yii::getAlias('@web') . '/img/pic2.jpg', ['target' => '_blank', 'alt' => 'PicNotFound', 'class' => 'icon-search']) ?>
-                                                    </div>
+                                                <div style="text-align: center; margin: 50px 20%;">
+                                                    <h4 style="color: #9e7b4e;">Sie suchen neuen Wohnraum?<br> Wie bieten Wohnobjekte aller Art!</h4>
+                                                    <hr class="no_line" style="margin: 0 auto 15px;">
+                                                    <p> Vom 1-Zimmer-Appartement für Studenten bis hin zu Luxus-Villas für gehobenere Ansprüche:<br>Kanat Immobilien ist der ideale Vermittlungsmarkler.  <?= Html::a(Yii::t('app', 'Kontaktieren'), ['/site/contact']) ?> Sie uns, oder inspizieren Sie unsere <?= Html::a(Yii::t('app', 'Angebote'), ['/site/index']) ?> <p>
                                                 </div>
                                             </div>
-                                        </div>
-                                    </li>
-                                    <li class="portfolio-item isotope-item  has-thumbnail">
-                                        <div class="portfolio-item-fw-bg" style="">
-                                            <div class="portfolio-item-fill"></div>
-                                            <div class="image_frame scale-with-grid">
-                                                <div class="image_wrapper">
-                                                    <div class="mask"></div>
-                                                    <?= Html::img('@web/img/singapore_skyscrapers-wallpaper-1680x1050.jpg', ['alt' => 'PicNotFound', 'class' => 'scale-with-grid wp-post-image']); ?>
-                                                    <div class="image_links double">
-                                                        <?= Html::a('', Yii::getAlias('@web') . '/img/pic2.jpg', ['target' => '_blank', 'alt' => 'PicNotFound', 'class' => 'icon-search']) ?>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                    </li>
-                                    <li class="portfolio-item isotope-item  has-thumbnail">
-                                        <div class="portfolio-item-fw-bg" style="">
-                                            <div class="portfolio-item-fill"></div>
-                                            <div class="image_frame scale-with-grid">
-                                                <div class="image_wrapper">
-                                                    <div class="mask"></div>
-                                                    <?= Html::img('@web/img/pic4.jpg', ['alt' => 'PicNotFound', 'class' => 'scale-with-grid wp-post-image']); ?>
-                                                    <div class="image_links double">
-                                                        <?= Html::a('', Yii::getAlias('@web') . '/img/pic4.jpg', ['target' => '_blank', 'alt' => 'PicNotFound', 'class' => 'icon-search']) ?>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <footer id="Footer" class="clearfix">
-        <div class="widgets_wrapper" style="padding:0 0 30px;">
-            <div class="container">
-                <div class="column one">
-                    <aside id="text-2" class="widget widget_text">
-                        <div class="textwidget">
-                            <div style="position: relative; margin-top: -100px; z-index: 1;">
-                                <div class="image_frame image_item no_link scale-with-grid aligncenter no_border" >
-                                    <div class="image_wrapper">
-                                        <?= Html::img('@web/img/erkan_logo.jpg', ['alt' => 'PicNotFound', 'class' => 'scale-with-grid', 'style' => 'width:350px;height:150px;']); ?>
+                                        </aside>
                                     </div>
                                 </div>
                             </div>
-                            <div style="text-align: center; margin: 50px 20%;">
-                                <h4 style="color: #9e7b4e;">Sie suchen neuen Wohnraum?<br> Wie bieten Wohnobjekte aller Art!</h4>
-                                <hr class="no_line" style="margin: 0 auto 15px;">
-                                <p> Vom 1-Zimmer-Appartement für Studenten bis hin zu Luxus-Villas für gehobenere Ansprüche:<br>Kanat Immobilien ist der ideale Vermittlungsmarkler.  <?= Html::a(Yii::t('app', 'Kontaktieren'), ['/site/contact']) ?> Sie uns, oder inspizieren Sie unsere <?= Html::a(Yii::t('app', 'Angebote'), ['/site/index']) ?> <p>
+                            <div class="footer_copy">
+                                <div class="container">
+                                    <div class="column one">
+                                        <div class="copyright"> &copy; 2018 Kanat Immobilien. All Rights Reserved.
+                                        </div>
+                                        <ul class="social"></ul>
+                                    </div>
+                                </div>
                             </div>
+
                         </div>
-                    </aside>
-                </div>
-            </div>
-        </div>
-        <div class="footer_copy">
-            <div class="container">
-                <div class="column one">
-                    <div class="copyright"> &copy; 2018 Kanat Immobilien. All Rights Reserved.
-                    </div>
-                    <ul class="social"></ul>
-                </div>
-            </div>
-        </div>
-    </footer>
-</div>
-<div id="Side_slide" class="right dark" data-width="250">
-    <div class="close-wrapper"><a href="#" class="close">
-            <i class="icon-cancel-fine">
-            </i>
-        </a>
-    </div>
-    <div class="extras">
-        <div class="extras-wrapper"></div>
-    </div>
-    <div class="lang-wrapper"></div>
-    <div class="menu_wrapper"></div>
-    <ul class="social"></ul>
-</div>
-<div id="body_overlay"></div>
+                        <div id="Side_slide" class="right dark" data-width="250">
+                            <div class="close-wrapper"><a href="#" class="close">
+                                    <i class="icon-cancel-fine">
+                                    </i>
+                                </a>
+                            </div>
+                            <div class="extras">
+                                <div class="extras-wrapper"></div>
+                            </div>
+                            <div class="lang-wrapper"></div>
+                            <div class="menu_wrapper"></div>
+                            <ul class="social"></ul>
+                        </div>
+                        <div id="body_overlay"></div>
 
 
