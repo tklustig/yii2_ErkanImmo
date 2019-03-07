614bb0ce350725e1eda1a28044327db6113f0fe2
Thomas Kipp <tklustig@gmail.com>
2019-03-07 19:22:13 +0100

    files in folder tests strukturiert

--- common/tests/unit/models/LoginFormTest.php
+++ common/tests/unit/models/LoginFormTest.php
@@ -6,22 +6,11 @@ use Yii;
 use common\models\LoginForm;
 use common\fixtures\UserFixture;
 
-/**
- * Login form test
- */
-class LoginFormTest extends \Codeception\Test\Unit
-{
-    /**
-     * @var \common\tests\UnitTester
-     */
+abstract class LoginFormTest extends \Codeception\Test\Unit {
+
     protected $tester;
 
-
-    /**
-     * @return array
-     */
-    public function _fixtures()
-    {
+    public function _fixtures() {
         return [
             'user' => [
                 'class' => UserFixture::className(),
@@ -30,8 +19,7 @@ class LoginFormTest extends \Codeception\Test\Unit
         ];
     }
 
-    public function testLoginNoUser()
-    {
+    public function testLoginNoUser() {
         $model = new LoginForm([
             'username' => 'not_existing_username',
             'password' => 'not_existing_password',
@@ -41,8 +29,7 @@ class LoginFormTest extends \Codeception\Test\Unit
         expect('user should not be logged in', Yii::$app->user->isGuest)->true();
     }
 
-    public function testLoginWrongPassword()
-    {
+    public function testLoginWrongPassword() {
         $model = new LoginForm([
             'username' => 'bayer.hudson',
             'password' => 'wrong_password',
@@ -53,8 +40,7 @@ class LoginFormTest extends \Codeception\Test\Unit
         expect('user should not be logged in', Yii::$app->user->isGuest)->true();
     }
 
-    public function testLoginCorrect()
-    {
+    public function testLoginCorrect() {
         $model = new LoginForm([
             'username' => 'bayer.hudson',
             'password' => 'password_0',
@@ -64,4 +50,5 @@ class LoginFormTest extends \Codeception\Test\Unit
         expect('error message should not be set', $model->errors)->hasntKey('password');
         expect('user should be logged in', Yii::$app->user->isGuest)->false();
     }
+
 }
