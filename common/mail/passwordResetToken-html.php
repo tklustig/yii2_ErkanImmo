774f61776783509f36bf6bb38aad8012c7f7fea4
Thomas Kipp <tklustig@gmail.com>
2019-03-07 19:19:25 +0100

    files in folder mail strukturiert

--- common/mail/passwordResetToken-html.php
+++ common/mail/passwordResetToken-html.php
@@ -1,8 +1,6 @@
 <?php
-use yii\helpers\Html;
 
-/* @var $this yii\web\View */
-/* @var $user common\models\User */
+use yii\helpers\Html;
 
 $resetLink = Yii::$app->urlManager->createAbsoluteUrl(['site/reset-password', 'token' => $user->password_reset_token]);
 ?>
--- common/mail/passwordResetToken-text.php
+++ common/mail/passwordResetToken-text.php
@@ -1,8 +1,4 @@
 <?php
-
-/* @var $this yii\web\View */
-/* @var $user common\models\User */
-
 $resetLink = Yii::$app->urlManager->createAbsoluteUrl(['site/reset-password', 'token' => $user->password_reset_token]);
 ?>
 Hello <?= $user->username ?>,
