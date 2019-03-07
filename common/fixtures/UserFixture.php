bee92e943dc1f6569f8f8b267cc86efd77af62c5
Thomas Kipp <tklustig@gmail.com>
2019-03-07 19:17:09 +0100

    file sturkturiert

--- common/fixtures/UserFixture.php
+++ common/fixtures/UserFixture.php
@@ -1,9 +1,11 @@
 <?php
+
 namespace common\fixtures;
 
 use yii\test\ActiveFixture;
 
-class UserFixture extends ActiveFixture
-{
+class UserFixture extends ActiveFixture {
+
     public $modelClass = 'common\models\User';
+
 }
