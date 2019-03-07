36d4643debd7bb53c0c8c8ae8c0b24ab4b6eddea
Thomas Kipp <tklustig@gmail.com>
2019-03-07 19:16:29 +0100

    config files strukturiert

--- common/config/params.php
+++ common/config/params.php
@@ -3,6 +3,6 @@
 return [
     'adminEmail' => 't.kipp@eisvogel-online-software.de',
     //'adminEmail' => 'tklustig.thomas@gmail.com',
-    'supportEmail' => 'support@example.com',
+    'supportEmail' => 'tklustig.thomas@gmail.com',
     'user.passwordResetTokenExpire' => 3600,
 ];
