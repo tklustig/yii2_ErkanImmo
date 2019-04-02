ab2f996b1a93e897e80ea9e599dd35e0f87c92d6
Thomas Kipp <t.kipp@eisvogel-onlnie-software.de>
2019-04-02 16:52:42 +0200

    Tracked

---
+++ console/migrations/insert.cmd
@@ -0,0 +1,13 @@
+@ECHO off
+REM for instance D:
+SET /p folder=Enter partition from mysql:  
+%folder%
+REM for instance D:\xampp\mysql\bin 
+SET /p path2Mysq=Enter path to mysql: 
+CD %path2Mysq%
+SET /p nameOfDump=Enter name of dump:
+REM with backslash after last folder 
+SET /p path2dump=Enter path to dump: 
+Set /p database=Enter name of database:
+mysql -u root %database% < %path2dump%%nameOfDump%
+pause
