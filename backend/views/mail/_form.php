ff807c12168ae3370d62c7199127e31b4003859d
Thomas Kipp <t.kipp@eisvogel-onlnie-software.de>
2019-04-05 09:32:07 +0200

    Mail verschicken -Teil 1

--- backend/views/mail/_form.php
+++ backend/views/mail/_form.php
@@ -1,47 +1,74 @@
 <?php
 
 use yii\helpers\Html;
-use yii\widgets\ActiveForm;
-
-/* @var $this yii\web\View */
-/* @var $model backend\models\Mail */
-/* @var $form yii\widgets\ActiveForm */
-
+use kartik\widgets\ActiveForm;
 ?>
 
 <div class="mail-form">
 
-    <?php $form = ActiveForm::begin(); ?>
+    <?php
+    $form = ActiveForm::begin([
+                'id' => 'dynamic-form',
+                'type' => ActiveForm::TYPE_VERTICAL,
+                'formConfig' => [
+                    'showLabels' => false
+                ]
+    ]);
+    ?>
 
     <?= $form->errorSummary($model); ?>
+    <div class="row">
+        <div class="col-md-3">
+            <?= $form->field($model, 'mail_from', ['addon' => [
+                    'prepend' => ['content' => 'Mailabsender']]])->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-md-3">
+            <?= $form->field($model, 'mail_to', ['addon' => [
+                    'prepend' => ['content' => 'Mainempfänger']]])->textInput(['maxlength' => true, 'placeholder' => 'mult. Empfänger durch Semikolon trennen']) ?>
+        </div>
+        <div class="col-md-3">
+            <?= $form->field($model, 'mail_cc', ['addon' => [
+                    'prepend' => ['content' => 'Cc Empfänger']]])->textInput(['maxlength' => true, 'placeholder' => 'mult. Empfänger durch Semikolon trennen']) ?>
+        </div>
+        <div class="col-md-3">
+            <?= $form->field($model, 'mail_bcc', ['addon' => [
+                    'prepend' => ['content' => 'Bcc Empfänger']]])->textInput(['maxlength' => true, 'placeholder' => 'mult. Empfänger durch Semikolon trennen'])
+            ?>
+        </div>
+        <div class="col-md-6">
+            <?=
+            $form->field($model, 'betreff', ['addon' => [
+                    'prepend' => ['content' => 'Betreff']]])->textInput(['maxlength' => true])
+            ?>
+        </div>
+        <div class="col-md-6">
 
-    <?= $form->field($model, 'id')->textInput(['placeholder' => 'Id']) ?>
-
-    <?= $form->field($model, 'id_mailserver')->widget(\kartik\widgets\Select2::classname(), [
-        'data' => \yii\helpers\ArrayHelper::map(\backend\models\Mailserver::find()->orderBy('id')->asArray()->all(), 'id', 'id'),
-        'options' => ['placeholder' => Yii::t('app', 'Choose Mailserver')],
-        'pluginOptions' => [
-            'allowClear' => true
-        ],
-    ]); ?>
-
-    <?= $form->field($model, 'mail_from')->textInput(['maxlength' => true, 'placeholder' => 'Mail From']) ?>
-
-    <?= $form->field($model, 'mail_to')->textInput(['maxlength' => true, 'placeholder' => 'Mail To']) ?>
-
-    <?= $form->field($model, 'mail_cc')->textInput(['maxlength' => true, 'placeholder' => 'Mail Cc']) ?>
-
-    <?= $form->field($model, 'mail_bcc')->textInput(['maxlength' => true, 'placeholder' => 'Mail Bcc']) ?>
-
-    <?= $form->field($model, 'betreff')->textInput(['maxlength' => true, 'placeholder' => 'Betreff']) ?>
-
-    <?= $form->field($model, 'bodytext')->textarea(['rows' => 6]) ?>
+            <?=
+            $form->field($model, 'id_mailserver', ['addon' => [
+                    'prepend' => ['content' => 'Mailserver'], 'append' => ['content' => 'mit diesem Server wird die Mail verschickt']]])->widget(\kartik\widgets\Select2::classname(), [
+                'data' => \yii\helpers\ArrayHelper::map(\backend\models\Mailserver::find()->orderBy('id')->asArray()->all(), 'id', 'serverHost'),
+                'options' => ['placeholder' => Yii::t('app', 'Mailserver wählen')],
+                'pluginOptions' => [
+                    'allowClear' => true
+                ],
+            ]);
+            ?>
+        </div>
+        <div class="col-md-12">
+            <?=
+            $form->field($model, 'bodytext', ['addon' => [
+                    'prepend' => ['content' => 'Mailinhalt']]])->widget(\dosamigos\ckeditor\CKEditor::className(), [
+                'preset' => 'full', 'clientOptions' => ['height' => 400],
+            ])
+            ?>
+        </div>
+    </div>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
-        <?= Html::a(Yii::t('app', 'Cancel'), Yii::$app->request->referrer , ['class'=> 'btn btn-danger']) ?>
+<?= Html::a(Yii::t('app', 'Cancel'), Yii::$app->request->referrer, ['class' => 'btn btn-danger']) ?>
     </div>
 
-    <?php ActiveForm::end(); ?>
+<?php ActiveForm::end(); ?>
 
 </div>
