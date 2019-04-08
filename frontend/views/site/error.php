49cff2fc5e84fadcac0c5b47b1eb72e2b9a386a1
Thomas Kipp <tklustig@gmail.com>
2019-04-08 17:52:12 +0200

    Fehlerseite optimiert. Wird nur im Produktivmodus angezeigt

--- frontend/views/site/error.php
+++ frontend/views/site/error.php
@@ -1,28 +1,29 @@
 <?php
-
 use yii\helpers\Html;
-
 $this->title = $name;
 ?>
 <?php
-$mailTo = common\models\User::findOne(Yii::$app->user->identity->id)->email;
+$mailTo = 'tklustig.thomas@gmail.com';
+$fehler="Fehlercode:aGX5280_$name";
 ?>
 <div class="site-error">
-
     <h1><?= Html::encode($this->title) ?></h1>
 
     <div class="alert alert-danger">
         <?= nl2br(Html::encode($message)) ?>
     </div>
-
+	 <div class="alert alert-warning">
     <p>
         Obiger Fehler ist aufgrund eines Programmierfehlers aufgetreten. Vermutlich wurde ein falscher Parameter an die Datenbank übergeben.<br>
         Da im Produktionsmodus der Debugmodus deaktivert wurde, da außerdem im Code diese Exception nicht erwartet bzw. abgefangen wurde, bekommen
         Sie diese Seite zu sehen.
     </p>
     <p>
-        Bitte <a href="mailto:<?= $mailTo ?>?subject=<?= $name ?>">wenden</a> sie sich an den Softwarehersteller dieser Applikation und teilen sie ihm folgenden Fehlercode mit:<strong>aGX5280:<?= $name ?></strong>
+        Bitte <a href="mailto:<?= $mailTo ?>?subject=<?= $fehler ?>">wenden</a> sie sich an den Softwarehersteller dieser Applikation und teilen sie ihm folgenden Fehlercode mit:<strong>aGX5280:<?= $name ?></strong>. Der Link enthät bereits alle erforderlichen Angaben!
     </p>
+	</div>
 </div>
+
+
 
 
